08a7c34da385cee960c13050d757fb788ff15c19
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-07 23:50:58 +0700

    component View with styles

--- App.js
+++ App.js
@@ -1,24 +1,25 @@
-import React from "react"
-import { Text, View, ViewComponent } from "react-native"
+import React from 'react';
+import { Text, View, ViewComponent } from 'react-native';
 
 const App = () => {
   // 1.) Create New Components(Functional Components)
   return (
     <View>
-    {/* View adalah parent apabaila hanya teks makan tidak bisa
+      {/* View adalah parent apabaila hanya teks makan tidak bisa
     di eksekusi */}
+
+      <View style={{ with:80, height:80, backgroundColor: '#93CAED'  }} />
+
       <Text>Rayyyyy</Text>
       <Text>Rayyyyy</Text>
-      <Clouse/>
+      <Clouse />
       <Text>Rayyyyy</Text>
     </View>
   );
-    
 };
 
 const Clouse = () => {
   return <Text>Bilaaa</Text>;
 };
-
 
 export default App;
