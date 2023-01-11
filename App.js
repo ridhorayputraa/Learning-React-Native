8ad0851fffb5dc8eca316765f4c90eb1bbe66682
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-11 20:39:08 +0700

    done styling chalenge

--- App.js
+++ App.js
@@ -30,7 +30,39 @@ return (
   {/* CHallnge styling */}
   <View style={{ padding: 12, backgroundColor: '#f2f2f2', width: 212, borderRadius: 8 }}>
     <Image source={mackbook} style={{ width: 188, height: 107, borderRadius: 8 }} />
+
+    <Text style={{ fontSize: 14, fontWeight:'bold', marginTop: 16 }}>
+      New Macbook Pro 2019
+    </Text>
+
+    <Text style={{ fontSize: 12, fontWeight: 'bold', color: '#f2994a', marginTop: 12 }}>Rp. 25.000.</Text>
+  
+        <Text style={{ fontSize: 12, fontWeight: '300', marginTop:12 }}>
+          Jakarta Barat
+        </Text>
+
+        {/* Button */}
+        <View 
+        style={{ 
+          backgroundColor: '#6fcf97',
+          paddingVertical: 6,
+          borderRadius: 25,
+          marginTop: 20
+         }}>
+          <Text
+          style={{ 
+            fontSize: 14,
+            fontWeight: '600',
+            color:'white',
+            textAlign:'center'
+           }}
+          >BELI</Text>
+        </View>
+
   </View>
+
+   
+
 </View>
 );
 };
@@ -44,6 +76,7 @@ marginLeft: 10,
 marginTop: 40,
 },
 });
+
 
 const SampleComponent = () => {
 return (
