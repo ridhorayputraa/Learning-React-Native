64328e2018f6e73b725ccda42b45a711133facd0
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-13 05:38:02 +0700

    create sample case youtube layouting

--- MateriFlexbox.js
+++ MateriFlexbox.js
@@ -5,6 +5,7 @@ import { Text, View } from "react-native";
 class MateriFlexbox extends Component{
     render(){
         return (
+            <View>
           <View
             style={{
               flexDirection: 'row',
@@ -18,6 +19,18 @@ class MateriFlexbox extends Component{
             <View style={{ backgroundColor: '#1dd1a1', width: 50, height: 50 }}></View>
             <View style={{ backgroundColor: '#5f27cd', width: 50, height: 50 }}></View>
           </View>
+            <View style={{ flexDirection:'row', justifyContent: 'space-around' }}>
+
+            <Text>Beranda</Text>
+             <Text>Video</Text>
+             <Text>Playlist</Text>
+             <Text>Komunitas</Text>
+             <Text>Channel</Text>
+             <Text>Tentang</Text>
+            </View>
+            
+
+            </View>
         ); 
     }
 }
