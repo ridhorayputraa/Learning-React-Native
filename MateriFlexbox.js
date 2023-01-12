ec7c1fd9d800bfc263e0123a0d99f89a27ed670c
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-13 04:38:47 +0700

    creating squer

--- MateriFlexbox.js
+++ MateriFlexbox.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 
 
 class MateriFlexbox extends Component{
     render(){
-        return <Text>Materi Flex Box</Text>
+        return(
+            <View>
+            <Text>Materi Flex Box</Text>
+            <View style={{ backgroundColor:'#ee5253', width:100, height:100}} ></View>
+            <View style={{ backgroundColor: '#feca57', width:100, height:100}} ></View>
+            <View style={{ backgroundColor: '#1dd1a1',width:100, height:100}} ></View>
+            <View style={{ backgroundColor: '#5f27cd',width:100, height:100}} ></View>
+            </View>
+        ) 
     }
 }
 
