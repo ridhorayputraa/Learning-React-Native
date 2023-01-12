6e1c88282e0cee5c18eaf9aa4c1aa178e96c8e04
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-12 17:12:53 +0700

    import sample component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Image, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 import mackbook from './mackbook.jpg';
+import SampleComponent from './SampleComponent';
 
 const App = () => {
   // 1.) Create New Components(Functional Components)
