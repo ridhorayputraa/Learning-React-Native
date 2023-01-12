f07b76c4bf866e4d85edaa90f64d34beca17a54c
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-13 05:03:33 +0700

    flex width

--- MateriFlexbox.js
+++ MateriFlexbox.js
@@ -4,15 +4,21 @@ import { Text, View } from "react-native";
 
 class MateriFlexbox extends Component{
     render(){
-        return(
-            <View>
-            <Text>Materi Flex Box</Text>
-            <View style={{ backgroundColor:'#ee5253', width:100, height:100}} ></View>
-            <View style={{ backgroundColor: '#feca57', width:100, height:100}} ></View>
-            <View style={{ backgroundColor: '#1dd1a1',width:100, height:100}} ></View>
-            <View style={{ backgroundColor: '#5f27cd',width:100, height:100}} ></View>
-            </View>
-        ) 
+        return (
+          <View
+            style={{
+              flexDirection: 'row',
+              backgroundColor: '#c8d6e5',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}
+          >
+            <View style={{ backgroundColor: '#ee5253', width: 50, height: 50 }}></View>
+            <View style={{ backgroundColor: '#feca57', width: 50, height: 50 }}></View>
+            <View style={{ backgroundColor: '#1dd1a1', width: 50, height: 50 }}></View>
+            <View style={{ backgroundColor: '#5f27cd', width: 50, height: 50 }}></View>
+          </View>
+        ); 
     }
 }
 
