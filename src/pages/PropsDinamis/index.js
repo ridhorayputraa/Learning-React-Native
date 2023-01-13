1d5cb16b91930ca29dfb456f08ab521cfd1e354f
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-13 23:43:15 +0700

    creat story component

--- src/pages/PropsDinamis/index.js
+++ src/pages/PropsDinamis/index.js
@@ -1,10 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+
+const Story = () => {
+  return (
+    <>
+      <Text>Materi Component Dinamis dengan Props</Text>
+      <Image
+        source={{
+          uri: 'https://instagram.fcgk27-1.fna.fbcdn.net/v/t51.2885-15/234136810_190789293084602_7389807128971981256_n.jpg?stp=c0.280.720.720a_dst-jpg_e35_s150x150&_nc_ht=instagram.fcgk27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=fAVcPtfxIAUAX_E9wbP&tn=Tj0pwX5G0-xn_3WU&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AfC7MhPRfIa1xOKcWp7LgcD0jC8FmpcVyHC_d8nYrC7lLw&oe=63C36FE5&_nc_sid=acd11b',
+        }}
+        style={{ width: 50, height: 50 }}
+      />
+      <Text>Bros</Text>
+    </>
+  );
+};
 
 export default function index() {
   return (
     <View>
-      <Text>Materi Component Dinamis dengan Props</Text>
+      <Story />
+      <Story />
     </View>
   );
 }
