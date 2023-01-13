92ec4f87f198bdbe219e421c771c6318f3cdd470
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-13 23:52:16 +0700

    sending image to props

--- src/pages/PropsDinamis/index.js
+++ src/pages/PropsDinamis/index.js
@@ -1,15 +1,15 @@
 import { Image, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
-const Story = ({name}) => {
+const Story = ({ name, image }) => {
   return (
     <>
       <Text>Materi Component Dinamis dengan Props</Text>
       <Image
         source={{
-          uri: 'https://instagram.fcgk27-1.fna.fbcdn.net/v/t51.2885-15/234136810_190789293084602_7389807128971981256_n.jpg?stp=c0.280.720.720a_dst-jpg_e35_s150x150&_nc_ht=instagram.fcgk27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=fAVcPtfxIAUAX_E9wbP&tn=Tj0pwX5G0-xn_3WU&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AfC7MhPRfIa1xOKcWp7LgcD0jC8FmpcVyHC_d8nYrC7lLw&oe=63C36FE5&_nc_sid=acd11b',
+          uri:  image ,
         }}
-        style={{ width: 50, height: 50 }}
+        style={{ width: 50, height: 50, borderRadius: 50/2 }}
       />
       <Text>{name}</Text>
     </>
@@ -19,8 +19,14 @@ const Story = ({name}) => {
 export default function index() {
   return (
     <View>
-      <Story name='Ridhoooo' />
-      <Story name='bross' />
+      <Story
+        image="https://instagram.fcgk27-1.fna.fbcdn.net/v/t51.2885-15/234136810_190789293084602_7389807128971981256_n.jpg?stp=c0.280.720.720a_dst-jpg_e35_s150x150&_nc_ht=instagram.fcgk27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=fAVcPtfxIAUAX_E9wbP&tn=Tj0pwX5G0-xn_3WU&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AfC7MhPRfIa1xOKcWp7LgcD0jC8FmpcVyHC_d8nYrC7lLw&oe=63C36FE5&_nc_sid=acd11b"
+        name="Ridhoooo"
+      />
+      <Story
+        image="https://instagram.fcgk27-1.fna.fbcdn.net/v/t51.2885-15/234136810_190789293084602_7389807128971981256_n.jpg?stp=c0.280.720.720a_dst-jpg_e35_s150x150&_nc_ht=instagram.fcgk27-1.fna.fbcdn.net&_nc_cat=102&_nc_ohc=fAVcPtfxIAUAX_E9wbP&tn=Tj0pwX5G0-xn_3WU&edm=AGW0Xe4BAAAA&ccb=7-5&oh=00_AfC7MhPRfIa1xOKcWp7LgcD0jC8FmpcVyHC_d8nYrC7lLw&oe=63C36FE5&_nc_sid=acd11b"
+        name="bross"
+      />
     </View>
   );
 }
