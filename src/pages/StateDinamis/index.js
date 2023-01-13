f9667e8d334fa36d293d64ab075cd335f3f401e3
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-14 01:41:44 +0700

    adding counter component

--- src/pages/StateDinamis/index.js
+++ src/pages/StateDinamis/index.js
@@ -1,21 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const Counter = () => {
+  return (
+    <View>
+      <Text>0</Text>
+      <Button title="Tambah" />
+    </View>
+  );
+};
 
 const StateDinamis = () => {
   return (
     <View style={styles.wrapper}>
       <Text style={styles.textTitle}>Materi Component dengan state</Text>
+      <Counter />
     </View>
-  )
-}
+  );
+};
 
-export default StateDinamis
+export default StateDinamis;
 
 const styles = StyleSheet.create({
-    wrapper: {
-        padding:20
-    },
-    textTitle: {
-        textAlign: 'center'
-    }
-})
+  wrapper: {
+    padding: 20,
+  },
+  textTitle: {
+    textAlign: 'center',
+  },
+});
