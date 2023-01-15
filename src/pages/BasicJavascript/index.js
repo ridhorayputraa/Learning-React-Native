cb471e9ef9bb4c7983d28a3e6b0e01558ff8a9e1
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-15 12:25:17 +0700

    Tipe Data Primitive

--- src/pages/BasicJavascript/index.js
+++ src/pages/BasicJavascript/index.js
@@ -2,11 +2,26 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
 export default function BasicJavascript() {
+
+    // Tipe data
+    
+    // Primitive
+    const name = 'Ridho ray'; // string
+    let age = 19;  // number => Integer
+    const isMan = true; //boolean
+
+
   return (
-    <View>
-      <Text>BasicJavascript</Text>
+    <View style={styles.container}>
+      <Text style={styles.textTitle}>BasicJavascript</Text>
+
+      <Text>Name: {name}</Text>
+      <Text>AGe: {age}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: { padding: 20 },
+  textTitle: { textAlign: "center" },
+});
