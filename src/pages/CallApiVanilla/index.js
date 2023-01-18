0bf47a0ec0630e0ede574a2ec34821a191711369
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-19 02:53:00 +0700

    using another API for GET

--- src/pages/CallApiVanilla/index.js
+++ src/pages/CallApiVanilla/index.js
@@ -1,19 +1,20 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect } from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React, { useEffect } from "react";
 
 const CallApiVanilla = () => {
-    useEffect(() => {
-     fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-    }, [])
+  useEffect(() => {
+    //  fetch('https://jsonplaceholder.typicode.com/todos/1') => Json PlaceHolder
+    fetch("https://reqres.in/api/users/2")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <View>
       <Text>CallApiVanilla</Text>
     </View>
-  )
-}
+  );
+};
 
-export default CallApiVanilla
+export default CallApiVanilla;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
