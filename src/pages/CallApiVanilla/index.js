b9804e9f7de2df27d30d6994d010c20fddc7b26c
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-19 02:46:12 +0700

    using useEffect for fetch

--- src/pages/CallApiVanilla/index.js
+++ src/pages/CallApiVanilla/index.js
@@ -1,7 +1,12 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const CallApiVanilla = () => {
+    useEffect(() => {
+     fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+    }, [])
   return (
     <View>
       <Text>CallApiVanilla</Text>
