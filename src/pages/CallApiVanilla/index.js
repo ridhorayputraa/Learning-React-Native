f9444699eaa3e6a65fd660441b8ba150cf966148
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-19 03:44:16 +0700

    done showing data from API to component for "memunculkan Hasil Call API"

--- src/pages/CallApiVanilla/index.js
+++ src/pages/CallApiVanilla/index.js
@@ -1,7 +1,15 @@
 import { Button, Image, StyleSheet, Text, View } from "react-native";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const CallApiVanilla = () => {
+  // Buat data awal dengan STATE
+  const [dataUser, setDataUser] = useState({
+    avatar: "",
+    email: "",
+    first_name: "",
+    last_name: "",
+  });
+
   useEffect(() => {
     //  fetch('https://jsonplaceholder.typicode.com/todos/1') => Json PlaceHolder
     //     fetch("https://reqres.in/api/users/2")
@@ -32,16 +40,25 @@ const CallApiVanilla = () => {
   const getData = () => {
     fetch("https://reqres.in/api/users/2")
       .then((response) => response.json())
-      .then((json) => console.log(json));
+      .then((json) => {
+        console.log(json);
+        setDataUser(json.data);
+      });
   };
 
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>CallApiVanilla</Text>
+      {/* GET */}
       <Button title="GET DATA" onPress={getData} />
       <Text>Response GET DATA</Text>
-
+      {/* Munculkan data dari API */}
+      <Image source={{ uri: dataUser.avatar }} style={styles.avatar} />
+      <Text>{`${dataUser.first_name} ${dataUser.last_name}`}</Text>
+      <Text>{dataUser.email}</Text>
       <View style={styles.line} />
+
+      {/* Post */}
       <Button title="POST DATA" />
       <Text>Response POST DATA</Text>
     </View>
@@ -54,4 +71,5 @@ const styles = StyleSheet.create({
   container: { padding: 20 },
   textTitle: { textAlign: "center" },
   line: { height: 2, backgroundColor: "black", marginVertical: 20 },
+  avatar: { width: 100, height: 100, borderRadius: 100 },
 });
