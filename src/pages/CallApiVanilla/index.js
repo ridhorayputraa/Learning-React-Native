f1066de24c6835eb742c29ee6cd9db0a220f7a06
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-19 03:34:18 +0700

    fetch data with onPress Button for "memunculkan Hasil Call API"

--- src/pages/CallApiVanilla/index.js
+++ src/pages/CallApiVanilla/index.js
@@ -1,42 +1,46 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 import React, { useEffect } from "react";
 
 const CallApiVanilla = () => {
   useEffect(() => {
     //  fetch('https://jsonplaceholder.typicode.com/todos/1') => Json PlaceHolder
+    //     fetch("https://reqres.in/api/users/2")
+    //       .then((response) => response.json())
+    //       .then((json) => console.log(json));
+    //     //   Call API Method POST
+    //     const dataForApi = {
+    //       name: "morpheus",
+    //       job: "leader",
+    //     };
+    //     // Debug
+    //     console.log("Data Object : ", dataForApi);
+    //     console.log("Data Tringify ", JSON.stringify(dataForApi));
+    //     fetch("https://reqres.in/api/users", {
+    //       // Request Method
+    //       method: "POST",
+    //       headers: {
+    //         "Content-Type": "application/json",
+    //       },
+    //       body: JSON.stringify(dataForApi),
+    //     })
+    //       .then((response) => response.json())
+    //       .then((json) => {
+    //         console.log("Post Response: ", json);
+    //       });
+  }, []);
+
+  const getData = () => {
     fetch("https://reqres.in/api/users/2")
       .then((response) => response.json())
       .then((json) => console.log(json));
-
-    //   Call API Method POST
-    const dataForApi = {
-      name: "morpheus",
-      job: "leader",
-    };
-
-    // Debug
-    console.log("Data Object : ", dataForApi);
-    console.log("Data Tringify ", JSON.stringify(dataForApi));
-
-    fetch("https://reqres.in/api/users", {
-      // Request Method
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(dataForApi),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        console.log("Post Response: ", json);
-      });
-  }, []);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.textTitle}>CallApiVanilla</Text>
-      <Button title="GET DATA" />
+      <Button title="GET DATA" onPress={getData} />
       <Text>Response GET DATA</Text>
+
       <View style={styles.line} />
       <Button title="POST DATA" />
       <Text>Response POST DATA</Text>
