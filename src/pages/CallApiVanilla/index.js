8e4d3165c091ef9e7b070af5b81386b7a012a77c
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-01-19 03:06:07 +0700

    call API method POST

--- src/pages/CallApiVanilla/index.js
+++ src/pages/CallApiVanilla/index.js
@@ -7,7 +7,31 @@ const CallApiVanilla = () => {
     fetch("https://reqres.in/api/users/2")
       .then((response) => response.json())
       .then((json) => console.log(json));
+
+    //   Call API Method POST
+    const dataForApi = {
+      name: "morpheus",
+      job: "leader",
+    };
+
+    // Debug
+    console.log('Data Object : ', dataForApi);
+    console.log('Data Tringify ', JSON.stringify(dataForApi))
+    
+    fetch("https://reqres.in/api/users", {
+        // Request Method
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dataForApi)
+    })
+    .then(response => response.json())
+    .then(json => {
+        console.log('Post Response: ', json)
+    })
   }, []);
+
   return (
     <View>
       <Text>CallApiVanilla</Text>
